775251481e6a8f8d672a30b098d23e49a7bf8c5c
nian0114 <268078545@qq.com>
2016-04-21 10:59:37 +0800

    remove device assest

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -18,8 +18,3 @@ def FullOTA_PostValidate(info):
     info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
     info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/platform/msm_sdcc.1/by-name/system");');
     info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/platform/msm_sdcc.1/by-name/system");');
-
-def FullOTA_InstallEnd(info):
-    info.script.Mount("/system");
-    info.script.AppendExtra('assert(run_program("/tmp/install/bin/device_check.sh") == 0);');
-    info.script.Unmount("/system");
