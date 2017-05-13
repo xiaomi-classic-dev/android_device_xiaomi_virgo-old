2c6bee0ac7e2094b38867d0c73a5db70eb6f261b
GuaiYiHu <guaiyihu@foxmail.com>
2017-05-13 23:24:32 +0800

    virgo: update wlan symlink
    Change-Id: I608e59a9ee25595bf3f933050898fb5fb0667d1a

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -11,3 +11,9 @@ include build/core/generate_extra_images.mk
 
 # include the non-open-source counterpart to this file
 -include vendor/xiaomi/virgo/AndroidBoardVendor.mk
+
+# WiFi symlinks
+$(shell mkdir -p $(TARGET_OUT)/lib/modules; \
+        ln -sf /system/lib/modules/pronto/pronto_wlan.ko \
+               $(TARGET_OUT)/lib/modules/wlan.ko)
+# END WiFi symlinks
