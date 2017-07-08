a185319e13b8911dd112062e39ba29f8fe7f9e8c
GuaiYiHu <guaiyihu@foxmail.com>
2017-07-08 18:01:05 +0800

    Revert "virgo: add cancro kernel path"
    This reverts commit 88a86cf035cf02e947a0d11cde28316e03b0135b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,11 +46,10 @@ BOARD_GLOBAL_CPPFLAGS += -DUSE_RIL_VERSION_10
 BOARD_KERNEL_CMDLINE               := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive buildvariant=userdebug
 BOARD_KERNEL_SEPARATED_DT          := true
 BOARD_KERNEL_PREBUILT_DT           := true
-TARGET_PREBUILT_KERNEL             := $(LOCAL_PATH)/prebuilt/kernel
 BOARD_KERNEL_BASE                  := 0x00000000
 BOARD_KERNEL_PAGESIZE              := 2048
 BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
-TARGET_KERNEL_SOURCE               := kernel/xiaomi/cancro
+#TARGET_KERNEL_SOURCE               := kernel/xiaomi/cancro
 TARGET_KERNEL_ARCH                 := arm
 #TARGET_KERNEL_CONFIG               := mokee_cancro_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
