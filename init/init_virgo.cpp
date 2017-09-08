a6e8778dc0a2da63afb7e676c599309e0bbaec61
Bruno Martins <bgcngm@gmail.com>
2017-09-09 00:52:49 +0100

    virgo: init: Use core init function to read from files
    Change-Id: I3fd23490bcdfa4097dad73161ad226337e93cd18

--- init/init_virgo.cpp
+++ init/init_virgo.cpp
@@ -34,40 +34,19 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include <android-base/strings.h>
+
 #include "vendor_init.h"
 #include "log.h"
 #include "util.h"
 
-#define BUF_SIZE         64
-
-static int read_file2(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size - 1);
-    if ((rc > 0) && (rc < max_size))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
+using android::base::Trim;
 
 void init_alarm_boot_properties()
 {
-    char const *alarm_file = "/proc/sys/kernel/boot_reason";
-    char buf[BUF_SIZE];
-    if(read_file2(alarm_file, buf, sizeof(buf))) {
+    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
+    std::string boot_reason;
+    if (read_file(boot_reason_file, &boot_reason) {
 
     /*
      * Setup ro.alarm_boot value to true when it is RTC triggered boot up
@@ -84,7 +63,7 @@ void init_alarm_boot_properties()
      * 7 -> CBLPWR_N pin toggled (for external power supply)
      * 8 -> KPDPWR_N pin toggled (power key pressed)
      */
-        if(buf[0] == '3')
+        if (Trim(boot_reason) == "3")
             property_set("ro.alarm_boot", "true");
         else
             property_set("ro.alarm_boot", "false");
