e5e9a128c61a81c8a3b6676bb839f6862c8296af
GuaiYiHu <guaiyihu@foxmail.com>
2017-09-24 10:13:20 +0800

    virgo: rename light hal name to virgo
    Change-Id: Ia6f2f7cb9468356ebb990a68ee4e78ead1de3d63

--- liblight/lights.c
+++ liblight/lights.c
@@ -372,7 +372,7 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
     .version_major = 1,
     .version_minor = 0,
     .id = LIGHTS_HARDWARE_MODULE_ID,
-    .name = "cancro lights module",
+    .name = "virgo lights module",
     .author = "Google, Inc., CyanogenMod",
     .methods = &lights_module_methods,
 };
