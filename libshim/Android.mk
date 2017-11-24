728dc4841b183bfcb2d9cc3dcb4e519c7a72d195
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-24 18:23:24 +0800

    Revert "virgo: libshim: Shim tinymix and rename libshim_atomic to libshim_symbols"
    This reverts commit 7196b7848a5ff5bbffb276de389ecccebad47024.

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -1,12 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES:= external/tinyalsa/include
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_SRC_FILES := atomic.cpp mixer.c pcm.c
-LOCAL_MODULE := libshim_symbols
+LOCAL_SRC_FILES := atomic.cpp
+LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SHARED_LIBRARIES:= libcutils libutils
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Werror
 include $(BUILD_SHARED_LIBRARY)
