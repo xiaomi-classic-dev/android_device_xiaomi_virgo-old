27c15eb1df18a9256310c1204fc08609789c20b8
GuaiYiHu <guaiyihu@foxmail.com>
2017-12-26 22:23:32 +0800

    virgo: sensor: Use libsensors.leo.so
    Change-Id: I50aae16146842356d20ae316a4ed49b9d0f5d579

--- sensors/multihal.cpp
+++ sensors/multihal.cpp
@@ -472,7 +472,7 @@ static void lazy_init_modules() {
     // dlopen the module files and cache their module symbols in sub_hw_modules
     sub_hw_modules = new std::vector<hw_module_t *>();
     dlerror(); // clear any old errors
-    add_so_module("libsensors.st_mve.so");
+    add_so_module("libsensors.leo.so");
     pthread_mutex_unlock(&init_modules_mutex);
 }
 
